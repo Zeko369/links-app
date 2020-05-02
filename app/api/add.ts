64bcdc81f7a883374a6a0b475bfa6393076f9a17
Fran Zekan <zekan.fran369@gmail.com>
2020-05-03 01:04:02 +0200

    Add create api to use with extension

---
+++ app/api/add.ts
@@ -0,0 +1,29 @@
+import db from "db"
+import { NextApiRequest, NextApiResponse } from "next"
+import getTitle from "utils/titleHelper"
+
+const handler = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+  if (req.method !== "POST") {
+    res.status(404).send("Nothing to see here")
+    return
+  }
+
+  let { name, url } = req.body
+
+  if (url === undefined) {
+    res.status(400).json({ error: "no-url" })
+  }
+
+  if (name === undefined) {
+    name = getTitle(url)
+  }
+
+  try {
+    const link = await db.link.create({ data: { name, url } })
+    res.status(200).json(link)
+  } catch (err) {
+    res.status(500).json({ error: "create" })
+  }
+}
+
+export default handler
