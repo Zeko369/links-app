f4e487fc8cbca16d1600137b9a2d8eebdd5cc339
Fran Zekan <zekan.fran369@gmail.com>
2020-05-03 01:39:40 +0200

    Add keyboard shortcut

--- extension/background.js
+++ extension/background.js
@@ -3,26 +3,41 @@
 
 /// <reference path="./node_modules/@types/chrome/index.d.ts"/>
 
+const createNewLink = (url, name, tabId) => {
+  /** @type {RequestInit} */
+  const options = {
+    method: "POST",
+    body: JSON.stringify({ url, name }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }
+
+  fetch("https://links.zekan.tk/api/add", options)
+    .then((res) => {
+      return res.json()
+    })
+    .then((data) => {
+      console.log(data)
+      chrome.tabs.remove(tabId)
+    })
+}
+
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   if (message.type === "close") {
-    const { url, name } = message
+    const { url, name, id } = message
 
-    /** @type {RequestInit} */
-    const options = {
-      method: "POST",
-      body: JSON.stringify({ url, name }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
+    createNewLink(url, name, id)
+  }
+})
 
-    fetch("https://links.zekan.tk/api/add", options)
-      .then((res) => {
-        return res.json()
-      })
-      .then((data) => {
-        console.log(data)
-        chrome.tabs.remove(message.id)
-      })
+chrome.commands.onCommand.addListener(function (command) {
+  if (command === "save-this-link") {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      if (tabs.length === 1) {
+        const tab = tabs[0]
+        createNewLink(tab.url, undefined, tab.id)
+      }
+    })
   }
 })
