12d5fd46a479ee0709fef625da82c0558174c915
Fran Zekan <zekan.fran369@gmail.com>
2020-05-02 02:49:07 +0200

    Exit on esc

--- app/pages/links/[id]/index.tsx
+++ app/pages/links/[id]/index.tsx
@@ -1,8 +1,10 @@
 import Layout from "app/layouts"
 import Form from "app/components/Form"
 import { Link } from "@prisma/client"
-import { ssrQuery } from "blitz"
+import { ssrQuery, useRouter } from "blitz"
 import getLink from "app/queries/getLink"
+import useKeyPress from "app/hooks/useKeyPress"
+import { useEffect } from "react"
 
 interface ServerProps {
   link: Link
@@ -14,6 +16,15 @@ export const getServerSideProps = async ({ params, req, res }): Promise<{ props:
 }
 
 const Edit: React.FC<ServerProps> = ({ link }) => {
+  const router = useRouter()
+  const esc = useKeyPress("Escape")
+
+  useEffect(() => {
+    if (esc) {
+      router.push("/")
+    }
+  }, [router, esc])
+
   return (
     <Layout>
       <Form link={link} />
--- app/pages/links/add.tsx
+++ app/pages/links/add.tsx
@@ -1,7 +1,19 @@
 import Form from "app/components/Form"
 import Layout from "app/layouts"
+import { useRouter } from "blitz"
+import useKeyPress from "app/hooks/useKeyPress"
+import { useEffect } from "react"
 
 const Add: React.FC = () => {
+  const router = useRouter()
+  const esc = useKeyPress("Escape")
+
+  useEffect(() => {
+    if (esc) {
+      router.push("/")
+    }
+  }, [router, esc])
+
   return (
     <Layout>
       <Form />
