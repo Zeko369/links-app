ac529b0f1b47a2c0ee442fa1831d69bbf3da7524
Fran Zekan <zekan.fran369@gmail.com>
2020-05-03 01:09:16 +0200

    Add cors

--- app/api/add.ts
+++ app/api/add.ts
@@ -2,7 +2,25 @@ import db from "db"
 import { NextApiRequest, NextApiResponse } from "next"
 import getTitle from "utils/titleHelper"
 
+import Cors from "cors"
+
+const cors = Cors({ methods: ["GET", "POST"] })
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
 const handler = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+  await runMiddleware(req, res, cors)
+
   if (req.method !== "POST") {
     res.status(404).send("Nothing to see here")
     return
