f3862f2052528b6e5009c0b31afa1043da0fc52e
Fran Zekan <zekan.fran369@gmail.com>
2020-05-02 19:48:10 +0200

    Try catch wrapper around titleHelper, add google helper

--- utils/titleHelper.ts
+++ utils/titleHelper.ts
@@ -1,3 +1,5 @@
+import capitalize from "app/helpers/capitalize"
+
 const getTitle = (link: string): string => {
   let url: URL
   try {
@@ -7,28 +9,44 @@ const getTitle = (link: string): string => {
     return `{error}[${link}]`
   }
 
-  let hostname = url.hostname
+  try {
+    let hostname = url.hostname
 
-  if (hostname.startsWith("www")) {
-    hostname = hostname.slice(4)
-  }
-
-  if (url.pathname === "/") {
-    return hostname
-  }
-
-  switch (hostname) {
-    case "github.com": {
-      return `GITHUB:${url.pathname.slice(1)}`
+    if (hostname.startsWith("www")) {
+      hostname = hostname.slice(4)
     }
-    case "medium.com": {
-      // TODO: add medium detection with Sepia-Upstream
-      const [username, title] = url.pathname.slice(1).split("/")
-      return `MEDIUM:${username} -> ${title.split("-").slice(0, -1).join(" ")}`
+
+    if (url.pathname === "/") {
+      return hostname
     }
-    case "youtube.com":
-      // TODO: add youtube resolver
-      return `YOUTUBE:${url.search.slice(3)}`
+
+    switch (hostname) {
+      case "github.com": {
+        return `GITHUB:${url.pathname.slice(1)}`
+      }
+      case "medium.com": {
+        // TODO: add medium detection with Sepia-Upstream
+        if (url.pathname.slice(1).split("/").length === 1) {
+          return `MEDIUM-user: ${url.pathname.slice(1)}`
+        }
+
+        const [username, title] = url.pathname.slice(1).split("/")
+        return `MEDIUM:${username} -> ${title.split("-").slice(0, -1).join(" ")}`
+      }
+      case "youtube.com":
+        // TODO: add youtube resolver
+        return `YOUTUBE:${url.search.slice(3)}`
+      case "google.com": {
+        try {
+          const params = new URLSearchParams(url.search)
+          return `GOOGLE: ${capitalize(params.get("q"))}`
+        } catch (err) {
+          return `Google something`
+        }
+      }
+    }
+  } catch (err) {
+    console.error(err)
   }
 
   return url.pathname.split("/").filter(Boolean).reverse()[0]
