84c57d1db596f7b640f11cfadb7ea8b4df092e37
Fran Zekan <zekan.fran369@gmail.com>
2020-05-20 20:46:04 +0200

    Fix order

--- app/queries/getLinks.ts
+++ app/queries/getLinks.ts
@@ -1,7 +1,7 @@
 import db from "db"
 
 const getLinks = async () => {
-  const links = await db.link.findMany()
+  const links = await db.link.findMany({ orderBy: { id: "desc" } })
   return links
 }
 
