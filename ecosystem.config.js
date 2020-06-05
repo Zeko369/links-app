edd754f1315268e041fc74a16ba9035e3a26198c
Fran Zekan <zekan.fran369@gmail.com>
2020-06-06 01:17:47 +0200

    Add deploy scripts

---
+++ ecosystem.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  apps: [
+    {
+      name: "LinksApp",
+      script: "yarn",
+      args: "start --production",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: "zeko",
+      host: "167.86.117.186",
+      ref: "origin/master",
+      repo: "git@github.com:Zeko369/links-app.git",
+      path: "/home/zeko/deploy-links",
+      "post-deploy": "./scripts/postDeploy.sh",
+    },
+  },
+}
