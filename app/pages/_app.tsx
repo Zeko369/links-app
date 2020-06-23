7690db101bb369a494bfb7da11f0a20d5b7426ed
Fran Zekan <zekan.fran369@gmail.com>
2020-06-23 21:07:29 +0200

    Add page title, update blitz

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,9 +1,13 @@
+import { Head } from "blitz"
 import { ThemeProvider, CSSReset } from "@chakra-ui/core"
 import Layout from "app/layouts"
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider>
+      <Head>
+        <title>Links app</title>
+      </Head>
       <CSSReset />
       <Layout>
         <Component {...pageProps} />
