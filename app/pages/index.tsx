01969237bab060e98654cfe199aa31f75a5da1bb
Fran Zekan <zekan.fran369@gmail.com>
2020-09-23 23:42:42 +0200

    Add pagination

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,52 +1,69 @@
-import React, { useEffect } from "react"
-import { ssrQuery, useRouter } from "blitz"
-import { Link as ILink } from "@prisma/client"
-import { List, ListItem, Heading, Link as ChakraLink } from "@chakra-ui/core"
+import React, { useEffect, Suspense } from "react"
+import { useRouter, BlitzPage, useRouterQuery, usePaginatedQuery } from "blitz"
+import { List, ListItem, Heading, Stack, Flex, Button, Spinner, Input } from "@chakra-ui/core"
 import { Link } from "chakra-next-link"
 
 import getLinks from "app/queries/getLinks"
 import useKeyPress from "app/hooks/useKeyPress"
 
-const Links: React.FC<{ links: ILink[] }> = ({ links }) => {
+const ITEMS_PER_PAGE = 3
+
+const Links: React.FC = () => {
+  const router = useRouter()
+  const { page = 0, search = undefined } = useRouterQuery()
+  const [{ links, hasMore, count }] = usePaginatedQuery(getLinks, {
+    skip: ITEMS_PER_PAGE * Number(page),
+    take: ITEMS_PER_PAGE,
+    search: Array.isArray(search) ? search[0] : search,
+  })
+
+  const goToPreviousPage = () => router.push({ query: { page: Number(page) - 1 } })
+  const goToNextPage = () => router.push({ query: { page: Number(page) + 1 } })
+
   return (
     <>
-      <Heading>Links</Heading>
-      <Heading size="sm">{links.length} links</Heading>
-      <List styleType="circle">
-        {links.map((link) => (
-          <ListItem key={link.id} py={1}>
-            <Link href={`/links/[id]`} as={`/links/${link.id}`} wordBreak="break-all">
-              {link.name}
-            </Link>{" "}
-            -&gt;{" "}
-            <ChakraLink href={link.url} target="_blank" wordBreak="break-all">
-              {link.url}
-            </ChakraLink>
-          </ListItem>
-        ))}
-        {links.length === 0 && (
-          <Heading>
-            No links...
-            <span role="img" aria-label="cry">
-              😢
-            </span>
-          </Heading>
-        )}
-      </List>
+      <Flex justify="space-between" alignItems="center" mt="2">
+        <Heading>Links</Heading>
+        <Input maxW="400px" w="70%" placeholder="Search..." />
+      </Flex>
+      <Heading size="sm">{count} links</Heading>
+      {links.length > 0 ? (
+        <Stack mt="2">
+          <Stack isInline>
+            <Button isDisabled={page === 0} onClick={goToPreviousPage}>
+              Previous
+            </Button>
+            <Button isDisabled={!hasMore} onClick={goToNextPage}>
+              Next
+            </Button>
+          </Stack>
+          <List styleType="circle">
+            {links.map((link) => (
+              <ListItem key={link.id} py={1}>
+                <Link href={`/links/[id]`} as={`/links/${link.id}`} wordBreak="break-all">
+                  {link.name}
+                </Link>{" "}
+                -&gt;{" "}
+                <Link href={link.url} target="_blank" wordBreak="break-all">
+                  {link.url}
+                </Link>
+              </ListItem>
+            ))}
+          </List>
+        </Stack>
+      ) : (
+        <Heading>
+          No links...
+          <span role="img" aria-label="cry">
+            😢
+          </span>
+        </Heading>
+      )}
     </>
   )
 }
 
-interface ServerProps {
-  links: ILink[]
-}
-
-export const getServerSideProps = async ({ req, res }): Promise<{ props: ServerProps }> => {
-  const links = await ssrQuery(getLinks, {}, { req, res })
-  return { props: { links } }
-}
-
-const Home: React.FC<ServerProps> = ({ links }) => {
+const Home: BlitzPage = () => {
   const router = useRouter()
   const keyPress = useKeyPress("n")
 
@@ -56,7 +73,11 @@ const Home: React.FC<ServerProps> = ({ links }) => {
     }
   }, [keyPress, router])
 
-  return <Links links={links} />
+  return (
+    <Suspense fallback={<Spinner />}>
+      <Links />
+    </Suspense>
+  )
 }
 
 export default Home
--- app/queries/getLinks.ts
+++ app/queries/getLinks.ts
@@ -1,8 +1,35 @@
-import db from "db"
+import db, { FindManyLinkArgs } from "db"
 
-const getLinks = async () => {
-  const links = await db.link.findMany({ orderBy: { id: "desc" }, first: 30 })
-  return links
+interface GetLinkInput {
+  where?: FindManyLinkArgs["where"]
+  orderBy?: FindManyLinkArgs["orderBy"]
+  take?: FindManyLinkArgs["take"]
+  skip?: FindManyLinkArgs["skip"]
+  search?: string
+}
+
+const getLinks = async ({ where, orderBy, take, skip, search }: GetLinkInput) => {
+  const links = await db.link.findMany({
+    orderBy: { id: "desc", ...orderBy },
+    where: {
+      ...where,
+      OR: [
+        { description: { contains: search } },
+        { name: { contains: search } },
+        { url: { contains: search } },
+      ],
+    },
+    take,
+    skip,
+  })
+
+  const count = await db.link.count()
+  const hasMore = skip! + take! < count
+  return {
+    links,
+    hasMore,
+    count,
+  }
 }
 
 export default getLinks
